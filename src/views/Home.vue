b1241703a098d066dc35e783399bb3e4174a8f26
tolipov <ziyovuddin.developer@gmail.com>
2024-01-04 00:20:07 +0500

    get data from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,16 +4,21 @@
             <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
         </div>
         <div class="flex gap-2 mt-2 justify-center">
-            <router-link :to="{ name: 'byLetter',params:{letter} }" v-for="letter of letters" :key="letter">
+            <router-link :to="{ name: 'byLetter',params:{letter}}" v-for="letter of letters" :key="letter">
                 {{ letter }}
             </router-link>
         </div>
     </div>
 </template>
 <script setup>
-import { computed } from "vue";
+import { computed ,onMounted} from "vue";
 import store from "../store";
+import axiosClient from "../axiosClient.js";
 
-const meals = computed(() => store.state.meals);
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+
+onMounted( async () => {
+    const response = await axiosClient.get('/list.php?i=list');
+    console.log(response.data);
+})
 </script>
